85e859fc2264389719658f40b4ee708b600d5e3f
ar-baazigar <your-email@example.com>
2026-03-03 16:35:34 +0530

    Added Hacker Greeting and Notes

---
+++ 01-Python-Foundations/hacker_greeting.py
@@ -0,0 +1,5 @@
+alias = input ('Enter your hacker alias:')
+server_number = input('How many server are we hacking today')
+
+print(f"--- ACCESS GRANTED ---")
+print(f"Welcome, Agent {alias}. Initializing scan on {serverar__number} servers...")
